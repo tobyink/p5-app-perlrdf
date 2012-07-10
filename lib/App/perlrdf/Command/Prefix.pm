63e8fe2e74a97a0e6cdea8deb541c745344febbc
Toby Inkster <mail@tobyinkster.co.uk>
2012-07-10 11:53:37 +0100

    add pfx command

---
+++ lib/App/perlrdf/Command/Prefix.pm
@@ -0,0 +1,45 @@
+package App::perlrdf::Command::Prefix;
+
+use 5.010;
+use strict;
+use warnings;
+use utf8;
+
+BEGIN {
+	$App::perlrdf::Command::Prefix::AUTHORITY = 'cpan:TOBYINK';
+	$App::perlrdf::Command::Prefix::VERSION   = '0.001';
+}
+
+use App::perlrdf -command;
+
+use namespace::clean;
+
+use constant abstract      => q (Look up the full URIs for customary prefixes);
+use constant command_names => qw( prefix prefixes pfx );
+use constant description   => <<'DESCRIPTION';
+Given one or more prefixes such as "foaf", "dc", looks up the most
+commonly used full URI.
+DESCRIPTION
+use constant opt_spec     => (
+	[ 'format|f=s' => 'Output format ("turtle", "xmlns", "sparql", "text")' ],
+);
+
+sub execute
+{
+	my ($self, $opt, $arg) = @_;
+	require RDF::NS;
+	
+	my $method;
+	given ($opt->{format} // '')
+	{
+		when (/turtle|ttl/i)    { $method = 'TTL' }
+		when (/sparql/i)        { $method = 'SPARQL' }
+		when (/xmlns|xml/i)     { $method = 'XMLNS' }
+		default                 { $method = 'TXT' }
+	}
+	
+	say for RDF::NS->new('any')->$method(join q<,>, @$arg);
+}
+
+1;
+
