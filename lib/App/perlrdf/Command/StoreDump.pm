2d7fab2fe1515e6d149c21d918859c7603625d03
Toby Inkster <mail@tobyinkster.co.uk>
2012-07-11 13:29:14 +0100

    dump store command

---
+++ lib/App/perlrdf/Command/StoreDump.pm
@@ -0,0 +1,79 @@
+package App::perlrdf::Command::StoreDump;
+
+use 5.010;
+use strict;
+use warnings;
+use utf8;
+
+BEGIN {
+	$App::perlrdf::Command::StoreDump::AUTHORITY = 'cpan:TOBYINK';
+	$App::perlrdf::Command::StoreDump::VERSION   = '0.001';
+}
+
+use App::perlrdf -command;
+use namespace::clean;
+
+use constant abstract      => q (Dump RDF data from an RDF::Trine::Store.);
+use constant command_names => qw( store_dump dump );
+use constant description   => <<'INTRO' . __PACKAGE__->store_help . <<'DESCRIPTION';
+Dump data from an RDF::Trine::Store.
+INTRO
+
+Output files are specified the same way as for the 'translate' command. See
+'filespec' for more details.
+DESCRIPTION
+
+use constant opt_spec => (
+	__PACKAGE__->store_opt_spec,
+	[]=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>=>,
+	[ 'output|o=s@',       'Output filename or URL' ],
+	[ 'output-spec|O=s@',  'Output file specification (default: NQuads)' ],
+	[ 'output-format|s=s', 'Output format (mnemonic: serialise)' ],
+);
+
+sub execute
+{
+	require RDF::Trine;
+	require App::perlrdf::FileSpec::OutputRDF;
+	
+	my ($self, $opt, $arg) = @_;
+
+	$opt->{output_format} ||= 'NQuads';
+
+	my $store = $self->get_store($opt);
+	my $model = RDF::Trine::Model->new($store);
+
+	my @outputs = $self->get_filespecs(
+		'App::perlrdf::FileSpec::OutputRDF',
+		output => $opt,
+	);
+	
+	push @outputs, map {
+		App::perlrdf::FileSpec::OutputRDF->new_from_filespec(
+			$_,
+			$opt->{output_format},
+			$opt->{output_base},
+		)
+	} @$arg;
+	
+	push @outputs,
+		App::perlrdf::FileSpec::OutputRDF->new_from_filespec(
+			'-',
+			$opt->{output_format},
+			$opt->{output_base},
+		)
+		unless @outputs;
+	
+	for (@outputs)
+	{
+		printf STDERR "Writing %s\n", $_->uri;
+		
+		eval {
+			local $@ = undef;
+			$_->serialize_model($model);
+			1;
+		} or warn "$@\n";
+	}
+}
+
+1;
